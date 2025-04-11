920bcb022caa26ff0123050177d029d9518df880
Upeka Sandaruwan <145981195+upeka99@users.noreply.github.com>
2025-04-11 12:29:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const sendEmail = (e) => {
         
         <ul className="nav-links">
           <li className="active"><a href="#home">Home</a></li>
-          <li><a href="#about">About Me</a></li>
+          <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#contact">Contact</a></li>
